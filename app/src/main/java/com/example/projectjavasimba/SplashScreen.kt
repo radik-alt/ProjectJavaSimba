9c145a713d64456e5ff6096f5ab0955e22c0c3dd
Radik <Radik.app@yandex.ru>
2022-12-31 00:20:25 +0400

    добавил верстку toolbar, добавил зависимости, поставил стили

--- app/src/main/java/com/example/projectjavasimba/SplashScreen.kt
+++ app/src/main/java/com/example/projectjavasimba/SplashScreen.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 
 class SplashScreen : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
