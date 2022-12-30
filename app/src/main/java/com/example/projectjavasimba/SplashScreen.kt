7d96f5cde02c063d290e3fcc750d28980ab5340c
Radik <Radik.app@yandex.ru>
2022-12-30 22:08:14 +0400

    начала работы с версткой

---
+++ app/src/main/java/com/example/projectjavasimba/SplashScreen.kt
@@ -0,0 +1,16 @@
+package com.example.projectjavasimba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+
+    }
+}
