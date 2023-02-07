775f49f877a85f22335b22f17eaf013bb2060cfd
Radik <Radik.app@yandex.ru>
2023-02-07 23:02:20 +0400

    8 блок - добавил функционал удаления и добавления картинки из галлереи

--- app/src/main/java/com/example/projectjavasimba/presentation/profileFragment/SharedViewModel.kt
+++ app/src/main/java/com/example/projectjavasimba/presentation/profileFragment/SharedViewModel.kt
@@ -8,10 +8,16 @@ import androidx.lifecycle.ViewModel
 class SharedViewModel : ViewModel() {
 
     private val uriImage = MutableLiveData<Uri?>(null)
-
+    private val deleteImage = MutableLiveData<Boolean>()
     fun setImage(image:Uri){
         uriImage.value = image
     }
 
     fun getImage(): MutableLiveData<Uri?> = uriImage
+
+    fun setIsDeleteImage(delete:Boolean){
+        deleteImage.value = delete
+    }
+    fun getIsDeleteImage() = deleteImage
+
 }
