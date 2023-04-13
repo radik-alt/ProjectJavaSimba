3c1c992c7513a2c2533a88cc2e0f0ce477361664
Radik <Radik.app@yandex.ru>
2023-04-14 00:41:56 +0400

    10 блок - добавил SearchViewPagerViewHolder

---
+++ app/src/main/java/com/example/projectjavasimba/presentation/searchFragment/SearchViewPagerViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.projectjavasimba.presentation.searchFragment
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SearchViewPagerViewModel: ViewModel() {
+
+    val searchMeeting = MutableLiveData<String>()
+}
